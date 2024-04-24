f266291c8642dc282ceac0628172e9aec251538b
Игорь <pitsko2017@yandex.ru>
2024-04-24 22:29:39 +0500

    Заголовок без меню

---
+++ src/UI/ViewModels/LoadingViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace NextGen.src.UI.ViewModels
+{
+    class LoadingViewModel : INotifyPropertyChanged
+    {
+        private string _statusMessage = "Инициализация...";
+        public string StatusMessage
+        {
+            get { return _statusMessage; }
+            set
+            {
+                _statusMessage = value;
+                OnPropertyChanged(nameof(StatusMessage));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task InitializeApplicationAsync()
+        {
+            StatusMessage = "Подключение к базе данных...";
+            await Task.Delay(1000); // Имитация длительной операции
+            // Здесь код подключения к БД
+
+            StatusMessage = "Загрузка интерфейса...";
+            await Task.Delay(1000); // Имитация загрузки интерфейса
+            // Здесь код загрузки интерфейса
+
+            StatusMessage = "Завершение настройки...";
+            await Task.Delay(1000); // Завершающие шаги
+            // Здесь код завершающих настроек
+
+            StatusMessage = "Готово!";
+        }
+    }
+}
---
+++ src/UI/Views/LoadingWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using NextGen.src.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NextGen.src.UI.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для LoadingWindow.xaml
+    /// </summary>
+    public partial class LoadingWindow : Window
+    {
+        public LoadingWindow()
+        {
+            InitializeComponent();
+            Loaded += async (s, e) => await ((LoadingViewModel)DataContext).InitializeApplicationAsync();
+        }
+    }
+
+}
