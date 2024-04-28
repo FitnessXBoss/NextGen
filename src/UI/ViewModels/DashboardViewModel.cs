9a09997e01927f187b78dd636a369a034beeb986
Игорь <pitsko2017@yandex.ru>
2024-04-28 08:23:47 +0500

    ОБНОВЛЕНИЯ ВСЕГО, ДОДЕЛАТЬ МЕНЮ ПРИСТУПИТЬ К АВТОМАТИЗАЦИИ

--- src/UI/ViewModels/DashboardViewModel.cs
+++ src/UI/ViewModels/DashboardViewModel.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NextGen.src.UI.Helpers;
+using NextGen.src.UI.Views;
+using System.Windows.Input;
+using System.Windows;
 
 namespace NextGen.src.UI.ViewModels
 {
     class DashboardViewModel
     {
+        public ICommand ChangeUserCommand { get; private set; }
+
+        public DashboardViewModel()
+        {
+            ChangeUserCommand = new RelayCommand(ChangeUser);
+        }
+
+        private void ChangeUser()
+        {
+            // Создание и открытие окна авторизации
+            AuthorizationWindow authWindow = new AuthorizationWindow();
+            authWindow.Show();
+
+            // Закрытие текущего окна
+            Application.Current.Dispatcher.Invoke(() => {
+                foreach (Window window in Application.Current.Windows)
+                {
+                    if (window.GetType() == typeof(DashboardWindow))
+                    {
+                        window.Close();
+                    }
+                }
+            });
+        }
+
     }
 }
